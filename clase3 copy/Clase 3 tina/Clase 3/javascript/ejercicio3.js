ef7ac1bb0774fedd465cc553f7266b06db4d523e
torresangulovalentina-bit <torresangulovalentina@gmail.com>
2025-08-29 13:38:29 +0000

    g

---
+++ clase3 copy/Clase 3 tina/Clase 3/javascript/ejercicio3.js
@@ -0,0 +1,32 @@
+const categorias = [
+	{
+		titulo: 'Ropa para Hombre',
+		imgAlt: 'Ropa para Hombre',
+		link: 'hombres.html',
+		linkText: 'Ver más'
+	},
+	{
+		titulo: 'Ropa para Mujeres',
+		imgAlt: 'Ropa para Mujeres',
+		link: 'mujeres.html',
+		linkText: 'Ver más'
+	},
+	{
+		titulo: 'Accesorios',
+		imgAlt: 'Accesorios',
+		link: 'accesorios.html',
+		linkText: 'Ver más'
+	}
+];
+
+function crearArticulo(categoria) {
+	return `
+		<article class="categoria">
+			<h3>${categoria.titulo}</h3>
+			<div class="img-categoria">
+				<img src="./imgs/ropa1.jpg" alt="${categoria.imgAlt}" width="300px">
+			</div>
+			<a href="${categoria.link}">${categoria.linkText}</a>
+		</article>
+	`;
+}

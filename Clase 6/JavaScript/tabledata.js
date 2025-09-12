14bde620eda1fe7eb18f4b78e8e544a39e842e87
torresangulovalentina-bit <torresangulovalentina@gmail.com>
2025-09-12 14:01:09 +0000

    d

--- Clase 6/JavaScript/tabledata.js
+++ Clase 6/JavaScript/tabledata.js
@@ -2,7 +2,7 @@ const tableBodyData = [
     { name: "Lael Greer", position: "Systems Administrator", office: "London", salary: "$103,500" },
     { name: "Jonas Alexander", position: "Developer", office: "San Francisco", salary: "$86,500" },
     { name: "Shad Decker", position: "Regional Director", office: "Edinburgh", salary: "$183,000" },
-    
+
 ];
 
 function loadtableData() {
@@ -20,3 +20,16 @@ function loadtableData() {
     })
 }
 
+function loadtabledata() {}
+            const tableBody = document.getElementById("tableBodyData");
+            tableBody.innerHTML = "";
+            tableBodyData.forEach(data => {
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                    <td>${data.name}</td>
+                    <td>${data.position}</td>
+                    <td>${data.office}</td>
+                    <td>${data.salary}</td>
+                `;
+                tableBody.appendChild(row);
+            });

c8699d8e89ca09ee8f77b8bab2faf71cd9f7c077
torresangulovalentina-bit <torresangulovalentina@gmail.com>
2025-09-18 22:00:55 +0000

    terminar

---
+++ Clase 6/JavaScript/basic5.js
@@ -0,0 +1,29 @@
+const tableBodyData = [
+{numero:"1",First:"Mark",Last:"Otto",Handle:"@mdo"},
+{numero:"2",First:"Jacob",Last:"Thornton",Handle:"@fat"},
+{numero:"3",First:"Larry",Last:"the Bird",Handle:"@twitter"},
+{numero:"4",First:"Jane",Last:"Smith",Handle:"@jsmith"},
+{numero:"5",First:"Charlie",Last:"Davis",Handle:"@charlied"}
+
+
+   
+];
+
+function loadTableData() {
+    const tableBody = document.getElementById('tablebasic5');
+    tableBody.innerHTML = '';
+
+    tableBodyData.forEach(data => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <th scope="row">${data.numero}</th>
+            <td>${data.First}</td>
+            <td>${data.Last}</td>
+            <td>${data.Handle}</td>
+        `;
+        tableBody.appendChild(row);
+    
+  
+    })
+}
+ 
